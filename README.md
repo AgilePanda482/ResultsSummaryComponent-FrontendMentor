f9fb0fb77e213fefb9a36b750b28923f41f1adc8
Sebastian Rico Vela <dsrv06@gmail.com>
2024-09-11 00:31:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Frontend Mentor - Results summary component solution
+
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). 
+
+## Table of contents
+- [Frontend Mentor - Results summary component solution](#frontend-mentor---results-summary-component-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+  - [Author](#author)
+
+## Overview
+This is a simple page to practice HTML, CSS and JavaScript. 
+The challenge comes from the Frontend Mentor page. 
+
+### Screenshot
+![Normal Design](./screenshots/pc.jpg)
+
+![Responsive Design](./screenshots/responsive.jpg)
+
+
+
+### Links
+- Solution URL: [Link](https://resultssummarycomponent-agilepanda482.netlify.app/)
+
+### Built with
+- HTML5
+- CSS
+- JavaScript
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+In this challenge, I learned mainly how to keep using CSS correctly to make it look as fluid and responsive as possible, as well as how to use HTML tags and improve my frontend JS usage.
+
+## Author
+- Frontend Mentor - [@AgilePanda482](https://www.frontendmentor.io/profile/AgilePanda482)
+- Linkedin - [@AgilePanda482](https://www.linkedin.com/in/AgilePanda482/)
---
+++ src/script.js
@@ -0,0 +1,71 @@
+const url = "data.json"
+
+const jsonInformation = async () => {
+    try{
+        const dataResult = await fetch(url)
+
+        if(!dataResult.ok) {
+            throw new Error("request error")
+        }
+
+        return dataResult.json()
+
+    } catch (err) {
+        console.error("INTERNAL SERVER ERROR: ", err)
+        return 1
+    }
+
+}
+
+const htmlLoyout = async () => {
+    const data = await jsonInformation()
+    const withoutParsed = JSON.stringify(data, null, 2)
+
+    const jsonData = JSON.parse(withoutParsed)
+    const sectionSummary = document.getElementById("section-summary")
+
+    jsonData.forEach(item => {
+        const itemArticle = document.createElement('article')
+        itemArticle.classList.add(item.category)
+
+        const itemFigure = document.createElement('figure')
+            const img = document.createElement('img')
+            img.src = item.icon
+
+            const description = document.createElement('p')
+            description.textContent = item.category
+
+        const itemDiv = document.createElement('div')
+            const score = document.createElement('p')
+            score.classList.add("highlight")
+            score.textContent = item.score
+
+            const slash = document.createElement('p')
+            slash.classList.add("numbers")
+            slash.textContent = "/"
+
+            const content = document.createElement('p')
+            content.classList.add("numbers")
+            content.textContent = "100"
+
+        itemArticle.appendChild(itemFigure)
+            itemFigure.appendChild(img)
+            itemFigure.appendChild(description)
+
+        itemArticle.appendChild(itemDiv)
+            itemDiv.appendChild(score)
+            itemDiv.appendChild(slash)
+            itemDiv.appendChild(content)
+
+        sectionSummary.appendChild(itemArticle)
+    });
+
+    const continueButton = document.createElement('p')
+    continueButton.classList.add("continue")
+    continueButton.textContent = "Continue"
+
+    sectionSummary.appendChild(continueButton)
+}
+
+
+htmlLoyout()
